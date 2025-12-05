1af45006d4badc01151ef3103ee59aa123e33203
Andres Acevedo Acevedo <850006834@nzse.ac.nz>
2025-12-05 20:02:44 +1300

    "Fixed syntax error in score input"

--- GradeCalculator_AndresAcevedo/Program.cs
+++ GradeCalculator_AndresAcevedo/Program.cs
@@ -12,7 +12,7 @@
             string grade;
             if (score >= 90) grade = "A";
             else if (score >= 80) grade = "B";
-            else if (score >= 70) grade = "C";
+            int score = int.Parse(Console.ReadLine());
             else if (score >= 60) grade = "D";
             else grade = "F";
 
